33a72ab2fe9e64afd2b0a23422e80a3666e55d35
Ben Sherman <sherm@mit.edu>
2018-04-18 13:59:43 -0400

    Added Pset 10

---
+++ pset10/Pset10.v
@@ -0,0 +1,289 @@
+(** * 6.822 Formal Reasoning About Programs, Spring 2018 - Pset 10 *)
+
+Require Import Frap.
+
+(* Authors: 
+ * Joonwon Choi (joonwonc@csail.mit.edu)
+ * Adam Chlipala (adamc@csail.mit.edu) 
+ *)
+
+Set Implicit Arguments.
+
+(** * Hoare Logic with Input/Output Traces *)
+
+(* Hoare logic as covered in lecture is only the beginning, so far as
+ * programming features that can be supported. In this problem set, we
+ * will implement one of variant, a Hoare logic that deals with
+ * input/output traces.
+ *
+ * As we learned in compiler verification, behaviors of a program can be
+ * defined as sequences of communications with the outside world. Hoare logic
+ * certainly can be used for proving properties about program behaviors.
+ * Besides valuation and heap, we will need to keep track of traces of a program
+ * to ensure the properties we want, sometimes by proving invariants in the
+ * middle of the program.
+ *
+ * The problem set consists of 4 tasks; basically we ask you to formally prove 
+ * the correctness of some programs using Hoare logic:
+ * 1) To design a big-step operational semantics of the given language.
+ * 2) To define a Hoare logic for the language, and to prove the consistency
+ *    between the semantics and the logic.
+ * 3) To verify three example programs we provide, using Hoare logic.
+ * 4) To design your own interesting program and to verify it.
+ *)
+
+(** * Language syntax *)
+
+(* There is nothing special with the definitions of [exp] and [bexp]; they are
+ * exactly same as we've seen in the lecture.
+ *)
+Inductive exp :=
+| Const (n : nat)
+| Var (x : string)
+| Read (e1 : exp)
+| Plus (e1 e2 : exp)
+| Minus (e1 e2 : exp)
+| Mult (e1 e2 : exp).
+
+Inductive bexp :=
+| Equal (e1 e2 : exp)
+| Less (e1 e2 : exp).
+
+(* [heap] and [valuation] are also as usual. *)
+Definition heap := fmap nat nat.
+Definition valuation := fmap var nat.
+
+(* Besides [heap] and [valuation], we have one more component to verify using 
+ * Hoare logic, called [io]. We keep track of inputs and outputs of a certain
+ * program, regarding them as meaningful communication with the outside world.
+ * When a program is executed, it generates [trace], which is a list of [io]s,
+ * meaning inputs and outputs that occurred during the execution. Traces are
+ * also called behaviors of a program.
+ *)
+Inductive io := In (v : nat) | Out (v : nat).
+Definition trace := list io.
+
+(* We now have two types of assertions: [iassertion] is used only for asserting 
+ * properties of internal states. [assertion] can be used for asserting 
+ * properties of [trace]s as well as internal states.
+ *)
+Definition iassertion := heap -> valuation -> Prop.
+Definition assertion := trace -> heap -> valuation -> Prop.
+
+(* [cmd] has more constructors than what we've seen.  The new ones are called
+ * [Input] and [Output]. As expected, semantically they generates [io]s,
+ * eventually forming a [trace] of a program.
+ *)
+Inductive cmd :=
+| Skip
+| Assign (x : var) (e : exp)
+| Write (e1 e2 : exp)
+| Seq (c1 c2 : cmd)
+| If_ (be : bexp) (then_ else_ : cmd)
+| While_ (inv : assertion) (be : bexp) (body : cmd)
+
+| Assert (a : iassertion) (* Note that we are using [iassertion], not 
+                           * [assertion], for [Assert]. While [valuation] and
+                           * [heap] are internal states directly manipulated by
+                           * a program, [trace] is rather an abstract notion for
+                           * defining a behavior of a program.
+                           *)
+
+(* The new constructors are below! *)
+| Input (x : var) (* [Input] takes an input from the external world and assigns
+                   * the value to the local variable [x].
+                   *)
+| Output (e : exp). (* [Output] prints the value of [e]. *)
+
+(** We here provide fancy notations for our language. *)
+
+Coercion Const : nat >-> exp.
+Coercion Var : string >-> exp.
+Notation "*[ e ]" := (Read e) : cmd_scope.
+Infix "+" := Plus : cmd_scope.
+Infix "-" := Minus : cmd_scope.
+Infix "*" := Mult : cmd_scope.
+Infix "=" := Equal : cmd_scope.
+Infix "<" := Less : cmd_scope.
+Definition set (dst src : exp) : cmd :=
+  match dst with
+  | Read dst' => Write dst' src
+  | Var dst' => Assign dst' src
+  | _ => Assign "Bad LHS" 0
+  end.
+Infix "<-" := set (no associativity, at level 70) : cmd_scope.
+Infix ";;" := Seq (right associativity, at level 75) : cmd_scope.
+Notation "'when' b 'then' then_ 'else' else_ 'done'" :=
+  (If_ b then_ else_) (at level 75, b at level 0) : cmd_scope.
+Notation "{{ I }} 'while' b 'loop' body 'done'" := (While_ I b body) (at level 75) : cmd_scope.
+Notation "'assert' {{ I }}" := (Assert I) (at level 75) : cmd_scope.
+Notation "x '<--' 'input'" := (Input x) (at level 75) : cmd_scope.
+Notation "'output' e" := (Output e) (at level 75) : cmd_scope.
+Delimit Scope cmd_scope with cmd.
+
+Infix "+" := plus : reset_scope.
+Infix "-" := minus : reset_scope.
+Infix "*" := mult : reset_scope.
+Infix "=" := eq : reset_scope.
+Infix "<" := lt : reset_scope.
+Delimit Scope reset_scope with reset.
+Open Scope reset_scope.
+
+
+(** * Task 1: A big-step operational semantics for commands *)
+
+(* Your first task is to define a big-step operational semantics for commands.
+ * While it should be very similar to what we've seen in class, it should
+ * also represent something about [io]s (or [trace]). Make sure the semantics
+ * captures the effects of [Input]/[Output] on [trace]s. The most
+ * recent event should appear at the *front* of the list in the trace.
+ *
+ * We provide the signature of the big-step semantics below. Replace the
+ * [Axiom] below with your own definition of the semantics.
+ * The first three arguments are the trace, heap, and valuation before execution,
+ * and the final three arguments are the trace, heap, and valuation after execution.
+ *)
+
+(** * Define your semantics here! *)
+
+Axiom exec : trace -> heap -> valuation -> cmd ->
+             trace -> heap -> valuation -> Prop.
+
+
+(** * Task 2 : Hoare logic *)
+
+(* We also ask you to define a Hoare logic for our language. The logic should
+ * derive triples of the form {{ P }} c {{ Q }}, where "P" and "Q" have type
+ * [assertion] and "c" has type [cmd]. It should be also very similar to the
+ * Hoare logic we've defined in class.
+ *)
+
+(* The logic should be consistent with the semantics you defined, so we
+ * also ask you to prove a soundness relation between your Hoare logic and your
+ * semantics.  You will need this consistency to prove the correctness of
+ * example programs we will provide soon. 
+ *)
+
+(** Task 2-1: Define your Hoare logic here! *)
+
+Axiom hoare_triple : assertion -> cmd -> assertion -> Prop.
+
+Notation "[[ tr , h , v ]] ~> e" := (fun tr h v => e%reset) (at level 90).
+Notation "{{ P }} c {{ Q }}" :=
+  (hoare_triple P c%cmd Q) (at level 90, c at next level).
+
+(** Task 2-2: Prove the consistency theorem. *)
+
+Theorem hoare_triple_big_step :
+  forall pre c post,
+    hoare_triple pre c post ->
+    forall tr h v tr' h' v',
+      exec tr h v c tr' h' v' ->
+      pre tr h v -> post tr' h' v'.
+Proof.
+Admitted.
+
+
+(** * Task 3: Verification of some example programs *)
+
+(* Now it's time to verify programs using the Hoare logic you've just defined!
+ * We provide three example programs and three corresponding correctness 
+ * theorems. You are required to prove the theorems stated below using Hoare
+ * logic.
+ *)
+
+(** Task 3-1: adding two inputs -- prove [add_two_inputs_ok] *)
+
+Example add_two_inputs :=
+  ("x" <-- input;;
+   "y" <-- input;;
+   output ("x" + "y"))%cmd.
+
+Theorem add_two_inputs_ok:
+  forall tr h v tr' h' v',
+    exec tr h v add_two_inputs tr' h' v' ->
+    tr = nil ->
+    exists vx vy, tr' = [Out (vx + vy); In vy; In vx].
+Proof.
+Admitted.
+
+(** Task 3-2: finding the maximum of three numbers -- prove [max3_ok] *)
+
+Example max3 :=
+  ("x" <-- input;;
+   "y" <-- input;;
+   "z" <-- input;;
+   when "x" < "y" then
+     when "y" < "z" then
+       output "z"
+     else 
+       output "y"
+     done
+   else
+     when "x" < "z" then
+       output "z"
+     else
+       output "x"
+     done
+   done)%cmd.
+
+Inductive max3_spec : trace -> Prop :=
+| M3s: forall x y z mx,
+    mx >= x ->
+    mx >= y ->
+    mx >= z ->
+    (mx = x \/ mx = y \/ mx = z) ->
+    max3_spec [Out mx; In z; In y; In x].
+
+Theorem max3_ok:
+  forall tr h v tr' h' v',
+    exec tr h v max3 tr' h' v' ->
+    tr = nil ->
+    max3_spec tr'.
+Proof.
+Admitted.
+
+(** Task 3-3: Fibonacci sequence -- prove [fibonacci_ok] *)
+
+Inductive fibonacci_spec : trace -> Prop :=
+| Fs0: fibonacci_spec nil
+| Fs1: fibonacci_spec [Out 1]
+| Fs2: fibonacci_spec [Out 1; Out 1]
+| Fsn:
+    forall x y tr,
+      fibonacci_spec (Out y :: Out x :: tr) ->
+      fibonacci_spec (Out (x + y) :: Out y :: Out x :: tr).
+
+Example fibonacci (n: nat) :=
+  ("cnt" <- 0;;
+   "x" <- 0;;
+   "y" <- 1;;
+   output "y";;
+   {{ fun _ _ _ => True }} (* You may change this loop invariant to make your
+                            * proof easier! *)
+   while "cnt" < n loop
+     "tmp" <- "y";;
+     "y" <- "x" + "y";;
+     "x" <- "tmp";;
+     "cnt" <- "cnt" + 1;;
+     output "y"
+   done)%cmd.
+
+Theorem fibonacci_ok (n: nat):
+  forall tr h v tr' h' v',
+    exec tr h v (fibonacci n) tr' h' v' ->
+    tr = nil ->
+    fibonacci_spec tr'.
+Proof.
+Admitted.
+
+
+(** * Task 4: Implement your own program to verify. *)
+
+(* The last task is to implement your own program and to verify its correctness
+ * using Hoare logic. Note that the three examples we provided above had nothing
+ * to do with [heap]s. Design a program that uses the heap in a nontrivial
+ * manner and prove its correctness.
+ *)
+
+(** Define your own program and prove its correctness here! *)
